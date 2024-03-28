42728dfed05d83ce13a37f79040c25aa4a2332fa
SSkkky <worte5633@gmail.com>
2024-03-28 14:08:01 +0900

    server

--- server.js
+++ server.js
@@ -18,5 +18,6 @@ mongoose.connect(process.env.MONGODB_URI)
 const guestbookRouter = require('./routes/GuestBook');
 app.use('/', guestbookRouter);
 
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`서버가 ${PORT}번 포트에서 실행 중입니다.`));
