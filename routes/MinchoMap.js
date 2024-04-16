72524c1658405c06743cd7c3485b04da8ea392ae
SSkkky <worte5633@gmail.com>
2024-04-16 17:50:25 +0900

    민초맵 restapi 로그인 토큰발행완료

--- routes/MinchoMap.js
+++ routes/MinchoMap.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const MinchoMap = require('../models/MinchoMap');
 const mongoose = require('mongoose');
-
+const axios = require('axios');
 
 router.route('/')
     .get(async (req, res) => {
@@ -31,6 +31,51 @@ router.route('/:id')
         await MinchoMap.findByIdAndUpdate(req.params.id, req.body, { new: true });
         const updateData = await MinchoMap.find({});
         res.send(updateData);
+    })
+
+router.route('/oauth/callback')
+    .get(async (req, res) => {
+        let query = req.query;
+        console.log('code = ', req.query.code)
+        console.log('id = ', process.env.CLIENT_ID)
+        console.log('uri = ', process.env.REDIRECT_URI)
+
+        if (!Object.prototype.hasOwnProperty.call(query, "code")) {
+            return res.status(400).send("invalid_code");
+        }
+
+        const formData = new FormData();
+        formData.append("grant_type", "authorization_code");
+        formData.append("client_id", process.env.CLIENT_ID);
+        formData.append("redirect_uri", process.env.REDIRECT_URI);
+        formData.append("code", req.query.code);
+
+        const config = {
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded;charset=utf-8"
+            }
+        };
+
+        axios.post("https://kauth.kakao.com/oauth/token",
+            {
+                grant_type: 'authorization_code',
+                client_id: process.env.CLIENT_ID,
+                code: req.query.code,
+                redirect_uri: process.env.REDIRECT_URI
+            }
+            , config)
+            .then(response => {
+                console.log('----------응답 성공---------')
+                console.log(response)
+                console.log(response.data.access_token)
+                // const tokenToJson = JSON.parse(response.body);
+                // res.json(tokenToJson);
+                // console.log(tokenToJson);
+            })
+            .catch(error => {
+                console.log('------코드는 발급되었는데 오류남--------')
+                console.log(error);
+            });
     })
 
 
